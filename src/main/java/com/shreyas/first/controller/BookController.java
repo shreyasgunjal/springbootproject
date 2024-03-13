38da21bfa280af330b8da57282ae6b5b9b7287e8
shreyasgunjal <shreyas.gunjal.sg@gmail.com>
2024-03-13 20:58:11 +0530

    Added Visual(Front-End part)using html

--- src/main/java/com/shreyas/first/controller/BookController.java
+++ src/main/java/com/shreyas/first/controller/BookController.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shreyas.first.modal.Book;
